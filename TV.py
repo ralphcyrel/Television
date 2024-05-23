828222c9b0ad5076c1758d44a2273e41fbbb7648
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-05-23 14:57:22 +0800

    creating a method for tv1

--- TV.py
+++ TV.py
@@ -1,5 +1,11 @@
 from test_tv import TV
+def main():
 #creating a code for the result for tv1
+    tv1 = TV()
+    tv1.turn_on()
+    tv1.set_volume(3)
+    tv1.set_channel(30)
+
 #print the result of tv1
 #creating a code for the result for tv2
 #print the result of tv2
