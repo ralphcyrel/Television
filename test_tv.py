57a2f1b6626e7b0c3c81fe7cac8af171a0eab731
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-05-23 14:48:24 +0800

    a method for decreasing the volume

--- test_tv.py
+++ test_tv.py
@@ -25,6 +25,9 @@ class TV:
         if self.on and self.volume_level < 7:
             self.volume_level += 1
 #creating a method for decreasing the volume
+    def volume_down(self):
+        if self.on and self.volume_level > 1:
+            self.volume_level -= 1
 #creating a method for channel
 #creating a method for channel (1-120 limit)
 #creating a method for increasing the channel
