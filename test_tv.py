4651a753b03a8dfc0e23089cb0f89ace2f7f8aa1
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-05-23 14:32:57 +0800

    added pseudocode

---
+++ test_tv.py
@@ -0,0 +1,14 @@
+#Ralph Cyrel B. Ronda
+#BSCPE 1-2
+#Television (class)
+
+#constructor
+#creating a method for turn on and off
+#creating a method for volume
+#creating a method for volume (1-7 limit)
+#creating a method for increasing the volume
+#creating a method for decreasing the volume
+#creating a method for channel
+#creating a method for channel (1-120 limit)
+#creating a method for increasing the channel
+#creating a method for decreasing the channel
