8545256ebdeb13a9a8601437bdc1f02d4cacb56d
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-05-23 14:39:39 +0800

    creating a constructor

--- test_tv.py
+++ test_tv.py
@@ -2,7 +2,13 @@
 #BSCPE 1-2
 #Television (class)
 
+class TV:
 #constructor
+    def __init__(self):
+        self.channel = 1
+        self.volume_level = 1
+        self.on = False
+
 #creating a method for turn on and off
 #creating a method for volume
 #creating a method for volume (1-7 limit)
